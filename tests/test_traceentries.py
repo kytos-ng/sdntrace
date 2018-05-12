43dc3205b752df0596e40f9167f74e8e9c964444
Jeronimo Bezerra <jab.jero@gmail.com>
2018-05-12 12:13:24 -0400

    Created a class to receive and process user's entries provided via REST. A unittest file was also created with 80 different tests.

---
+++ tests/test_traceentries.py
@@ -0,0 +1,501 @@
+"""
+    Test tracing.trace_entries
+"""
+import unittest
+from tracing.trace_entries import TraceEntries
+
+
+class TestDpid(unittest.TestCase):
+    """ Test all combinations for DPID """
+
+    def setUp(self):
+        self.trace_entries = TraceEntries()
+
+    def test_incorrect_dpid_int(self):
+        """ DPID cannot be integer """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dpid = 0
+
+    def test_incorrect_dpid_empty_str(self):
+        """ DPID cannot be an empty str """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dpid = ''
+
+    def test_incorrect_dpid_invalid_characters_plus(self):
+        """ DPID cannot be a special character (only ':' is allowed) """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dpid = '+'
+
+    def test_incorrect_dpid_invalid_characters_comma(self):
+        """ DPID cannot be a special character (only ':' is allowed) """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dpid = ','
+
+    def test_incorrect_dpid_invalid_length(self):
+        """ DPID cannot be longer than 16 without ':' """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dpid = '00000000000000001'
+
+    def test_correct_dpid_char(self):
+        """ Correct DPID with char """
+        self.trace_entries.dpid = '1'
+
+    def test_read_dpid_char(self):
+        """ Read DPID '1' """
+        self.test_correct_dpid_char()
+        self.assertEqual(self.trace_entries.dpid, '1')
+
+    def test_correct_dpid_complete(self):
+        """ Correct DPID with colons, numbers, and letters """
+        self.trace_entries.dpid = 'ab:cd:ef:ab:cd:ef:00:01'
+
+    def test_read_dpid_complete(self):
+        """ Read DPID '1' """
+        self.test_correct_dpid_complete()
+        self.assertEqual(self.trace_entries.dpid, 'ab:cd:ef:ab:cd:ef:00:01')
+
+
+class TestInPort(unittest.TestCase):
+    """ Test all combinations for IN_PORT """
+
+    def setUp(self):
+        self.trace_entries = TraceEntries()
+
+    def test_incorrect_in_port_char(self):
+        """ IN_PORT cannot be a char """
+        with self.assertRaises(ValueError):
+            self.trace_entries.in_port = '1'
+
+    def test_incorrect_in_port_negative(self):
+        """ IN_PORT cannot be negative """
+        with self.assertRaises(ValueError):
+            self.trace_entries.in_port = -1
+
+    def test_correct_in_port(self):
+        """ IN_PORT Correct"""
+        self.trace_entries.in_port = 1
+
+    def test_read_in_port(self):
+        """ Read DPID '1' """
+        self.test_correct_in_port()
+        self.assertEqual(self.trace_entries.in_port, 1)
+
+
+class TestDlSrc(unittest.TestCase):
+    """ Test all combinations for DL_SRC """
+
+    def setUp(self):
+        self.trace_entries = TraceEntries()
+
+    def test_incorrect_dl_src_int(self):
+        """ dl_src cannot be integer """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_src = 0
+
+    def test_incorrect_dl_src_empty_str(self):
+        """ dl_src cannot be an empty str """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_src = ''
+
+    def test_incorrect_dl_src_invalid_characters_plus(self):
+        """ dl_src only accepts ':' snd '-' """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_src = '+'
+
+    def test_incorrect_dl_src_invalid_characters_comma(self):
+        """ dl_src cannot be a special character (only ':' is allowed) """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_src = ','
+
+    def test_incorrect_dl_src_invalid_length(self):
+        """ dl_src cannot be longer than 16 without ':' """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_src = '000000000000000001'
+
+    def test_correct_dl_src_char(self):
+        """ Correct dl_src with char """
+        self.trace_entries.dl_src = 'ab:cd:ef:ab:cd:00'
+
+    def test_read_dl_src_char(self):
+        """ Read dl_src '1' """
+        self.test_correct_dl_src_char()
+        self.assertEqual(self.trace_entries.dl_src, 'ab:cd:ef:ab:cd:00')
+
+
+class TestDlDst(unittest.TestCase):
+    """ Test all combinations for DL_DST """
+
+    def setUp(self):
+        self.trace_entries = TraceEntries()
+
+    def test_incorrect_dl_dst_int(self):
+        """ dl_dst cannot be integer """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_dst = 0
+
+    def test_incorrect_dl_dst_empty_str(self):
+        """ dl_dst cannot be an empty str """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_dst = ''
+
+    def test_incorrect_dl_dst_invalid_characters_plus(self):
+        """ dl_dst only accepts ':' snd '-' """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_dst = '+'
+
+    def test_incorrect_dl_dst_invalid_characters_comma(self):
+        """ dl_dst cannot be a special character (only ':' is allowed) """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_dst = ','
+
+    def test_incorrect_dl_dst_invalid_length(self):
+        """ dl_dst cannot be longer than 16 without ':' """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_dst = '000000000000000001'
+
+    def test_correct_dl_dst_char(self):
+        """ Correct dl_dst with char """
+        self.trace_entries.dl_dst = 'ab:cd:ef:ab:cd:00'
+
+    def test_read_dl_dst_char(self):
+        """ Read dl_dst '1' """
+        self.test_correct_dl_dst_char()
+        self.assertEqual(self.trace_entries.dl_dst, 'ab:cd:ef:ab:cd:00')
+
+
+class TestDlVlan(unittest.TestCase):
+    """ Test all combinations for DL_VLAN """
+
+    def setUp(self):
+        self.trace_entries = TraceEntries()
+
+    def test_incorrect_dl_vlan_char(self):
+        """ dl_vlan cannot be a char """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_vlan = '1'
+
+    def test_incorrect_dl_vlan_negative(self):
+        """ dl_vlan cannot be negative """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_vlan = -1
+
+    def test_incorrect_dl_vlan_too_big(self):
+        """ dl_vlan cannot be bigger than 4095 """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_vlan = 4096
+
+    def test_correct_dl_vlan(self):
+        """ dl_vlan Correct"""
+        self.trace_entries.dl_vlan = 1
+
+    def test_read_dl_vlan(self):
+        """ Read DPID '1' """
+        self.test_correct_dl_vlan()
+        self.assertEqual(self.trace_entries.dl_vlan, 1)
+
+
+class TestDlType(unittest.TestCase):
+    """ Test all combinations for dl_type """
+
+    def setUp(self):
+        self.trace_entries = TraceEntries()
+
+    def test_incorrect_dl_type_char(self):
+        """ dl_type cannot be a char """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_type = '1'
+
+    def test_incorrect_dl_type_negative(self):
+        """ dl_type cannot be negative """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_type = -1
+
+    def test_incorrect_dl_type_too_big(self):
+        """ dl_type cannot be bigger than 4095 """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_type = 65536
+
+    def test_correct_dl_type(self):
+        """ dl_type Correct"""
+        self.trace_entries.dl_type = 1
+
+    def test_read_dl_type(self):
+        """ Read DPID '1' """
+        self.test_correct_dl_type()
+        self.assertEqual(self.trace_entries.dl_type, 1)
+
+
+class TestDlVlanPcp(unittest.TestCase):
+    """ Test all combinations for dl_vlan_pcp """
+
+    def setUp(self):
+        self.trace_entries = TraceEntries()
+
+    def test_incorrect_dl_vlan_pcp_char(self):
+        """ dl_vlan_pcp cannot be a char """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_vlan_pcp = '1'
+
+    def test_incorrect_dl_vlan_pcp_negative(self):
+        """ dl_vlan_pcp cannot be negative """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_vlan_pcp = -1
+
+    def test_incorrect_dl_vlan_pcp_too_big(self):
+        """ dl_vlan_pcp cannot be bigger than 7 """
+        with self.assertRaises(ValueError):
+            self.trace_entries.dl_vlan_pcp = 8
+
+    def test_correct_dl_vlan_pcp(self):
+        """ dl_vlan_pcp Correct"""
+        self.trace_entries.dl_vlan_pcp = 1
+
+    def test_read_dl_vlan_pcp(self):
+        """ Read DPID '1' """
+        self.test_correct_dl_vlan_pcp()
+        self.assertEqual(self.trace_entries.dl_vlan_pcp, 1)
+
+
+class TestNwTos(unittest.TestCase):
+    """ Test all combinations for nw_tos """
+
+    def setUp(self):
+        self.trace_entries = TraceEntries()
+
+    def test_incorrect_nw_tos_char(self):
+        """ nw_tos cannot be a char """
+        with self.assertRaises(ValueError):
+            self.trace_entries.nw_tos = '1'
+
+    def test_incorrect_nw_tos_negative(self):
+        """ nw_tos cannot be negative """
+        with self.assertRaises(ValueError):
+            self.trace_entries.nw_tos = -1
+
+    def test_incorrect_nw_tos_too_big(self):
+        """ nw_tos cannot be bigger than 7 """
+        with self.assertRaises(ValueError):
+            self.trace_entries.nw_tos = 8
+
+    def test_correct_nw_tos(self):
+        """ nw_tos Correct"""
+        self.trace_entries.nw_tos = 1
+
+    def test_read_nw_tos(self):
+        """ Read DPID '1' """
+        self.test_correct_nw_tos()
+        self.assertEqual(self.trace_entries.nw_tos, 1)
+
+
+class TestNwSrc(unittest.TestCase):
+    """ Test all combinations for NW_SRC """
+
+    def setUp(self):
+        self.trace_entries = TraceEntries()
+
+    def test_incorrect_nw_src_int(self):
+        """ nw_src cannot be integer """
+        with self.assertRaises(ValueError):
+            self.trace_entries.nw_src = 0
+
+    def test_incorrect_nw_src_empty_str(self):
+        """ nw_src cannot be an empty str """
+        with self.assertRaises(ValueError):
+            self.trace_entries.nw_src = ''
+
+    def test_incorrect_nw_src_invalid_characters_plus(self):
+        """ nw_src only accepts '.' """
+        with self.assertRaises(ValueError):
+            self.trace_entries.nw_src = '+'
+
+    def test_incorrect_nw_src_256(self):
+        """ Correct nw_src with char """
+        with self.assertRaises(ValueError):
+            self.trace_entries.nw_src = '0.0.0.256'
+
+    def test_correct_nw_src_zeroed(self):
+        """ Correct nw_src with char """
+        self.trace_entries.nw_src = '0.0.0.0'
+
+    def test_read_nw_src_char(self):
+        """ Read nw_src '1' """
+        self.test_correct_nw_src_zeroed()
+        self.assertEqual(self.trace_entries.nw_src, '0.0.0.0')
+
+    def test_correct_nw_src_255(self):
+        """ Correct nw_src with char """
+        self.trace_entries.nw_src = '255.255.255.255'
+
+    def test_read_nw_src_char_255(self):
+        """ Read nw_src '1' """
+        self.test_correct_nw_src_255()
+        self.assertEqual(self.trace_entries.nw_src, '255.255.255.255')
+
+
+class TestNwDst(unittest.TestCase):
+    """ Test all combinations for NW_DST """
+
+    def setUp(self):
+        self.trace_entries = TraceEntries()
+
+    def test_incorrect_nw_dst_int(self):
+        """ nw_dst cannot be integer """
+        with self.assertRaises(ValueError):
+            self.trace_entries.nw_dst = 0
+
+    def test_incorrect_nw_dst_empty_str(self):
+        """ nw_dst cannot be an empty str """
+        with self.assertRaises(ValueError):
+            self.trace_entries.nw_dst = ''
+
+    def test_incorrect_nw_dst_invalid_characters_plus(self):
+        """ nw_dst only accepts '.' """
+        with self.assertRaises(ValueError):
+            self.trace_entries.nw_dst = '+'
+
+    def test_incorrect_nw_dst_256(self):
+        """ Correct nw_dst with char """
+        with self.assertRaises(ValueError):
+            self.trace_entries.nw_dst = '0.0.0.256'
+
+    def test_correct_nw_dst_zeroed(self):
+        """ Correct nw_dst with char """
+        self.trace_entries.nw_dst = '0.0.0.0'
+
+    def test_read_nw_dst_char(self):
+        """ Read nw_dst '1' """
+        self.test_correct_nw_dst_zeroed()
+        self.assertEqual(self.trace_entries.nw_dst, '0.0.0.0')
+
+    def test_correct_nw_dst_255(self):
+        """ Correct nw_dst with char """
+        self.trace_entries.nw_dst = '255.255.255.255'
+
+    def test_read_nw_dst_char_255(self):
+        """ Read nw_dst '1' """
+        self.test_correct_nw_dst_255()
+        self.assertEqual(self.trace_entries.nw_dst, '255.255.255.255')
+
+
+class TestTpSrc(unittest.TestCase):
+    """ Test all combinations for tp_src """
+
+    def setUp(self):
+        self.trace_entries = TraceEntries()
+
+    def test_incorrect_tp_src_char(self):
+        """ tp_src cannot be a char """
+        with self.assertRaises(ValueError):
+            self.trace_entries.tp_src = '1'
+
+    def test_incorrect_tp_src_negative(self):
+        """ tp_src cannot be negative """
+        with self.assertRaises(ValueError):
+            self.trace_entries.tp_src = -1
+
+    def test_incorrect_tp_src_too_big(self):
+        """ tp_src cannot be bigger than 4095 """
+        with self.assertRaises(ValueError):
+            self.trace_entries.tp_src = 65536
+
+    def test_correct_tp_src(self):
+        """ tp_src Correct"""
+        self.trace_entries.tp_src = 1
+
+    def test_read_tp_src(self):
+        """ Read DPID '1' """
+        self.test_correct_tp_src()
+        self.assertEqual(self.trace_entries.tp_src, 1)
+
+
+class TestTpDst(unittest.TestCase):
+    """ Test all combinations for tp_dst """
+
+    def setUp(self):
+        self.trace_entries = TraceEntries()
+
+    def test_incorrect_tp_dst_char(self):
+        """ tp_dst cannot be a char """
+        with self.assertRaises(ValueError):
+            self.trace_entries.tp_dst = '1'
+
+    def test_incorrect_tp_dst_negative(self):
+        """ tp_dst cannot be negative """
+        with self.assertRaises(ValueError):
+            self.trace_entries.tp_dst = -1
+
+    def test_incorrect_tp_dst_too_big(self):
+        """ tp_dst cannot be bigger than 4095 """
+        with self.assertRaises(ValueError):
+            self.trace_entries.tp_dst = 65536
+
+    def test_correct_tp_dst(self):
+        """ tp_dst Correct"""
+        self.trace_entries.tp_dst = 1
+
+    def test_read_tp_dst(self):
+        """ Read DPID '1' """
+        self.test_correct_tp_dst()
+        self.assertEqual(self.trace_entries.tp_dst, 1)
+
+
+class TestLoadEntries(unittest.TestCase):
+    """ Now, load all entries at once"""
+
+    def setUp(self):
+        self.trace_entries = TraceEntries()
+
+    def test_missing_trace(self):
+        """ key trace is mandatory """
+        with self.assertRaises(ValueError):
+            entries = {}
+            self.trace_entries.load_entries(entries)
+
+    def test_wrong_trace(self):
+        """ key trace has to be a dict """
+        with self.assertRaises(ValueError):
+            entries = {'trace:': 0}
+            self.trace_entries.load_entries(entries)
+
+    def test_missing_switch(self):
+        """ key trace/switch is mandatory """
+        with self.assertRaises(ValueError):
+            entries = {'trace': {}}
+            self.trace_entries.load_entries(entries)
+
+    def test_missing_dpid(self):
+        """ key trace/switch is mandatory """
+        with self.assertRaises(ValueError):
+            dpid = {}
+            switch = {'switch': dpid}
+            entries = {'trace': switch}
+            self.trace_entries.load_entries(entries)
+
+    def test_missing_in_port(self):
+        """ key trace/switch is mandatory """
+        with self.assertRaises(ValueError):
+            dpid = {'dpid': 'a'}
+            switch = {'switch': dpid}
+            entries = {'trace': switch}
+            self.trace_entries.load_entries(entries)
+
+    def test_missing_eth(self):
+        """ key trace/switch is mandatory """
+        with self.assertRaises(ValueError):
+            eth = 0
+            dpid = {'dpid': 'a', 'in_port': 1}
+            switch = {'switch': dpid, 'eth': eth}
+            entries = {'trace': switch}
+            self.trace_entries.load_entries(entries)
+
+    def test_minimally_correct(self):
+        """ key trace/switch is mandatory """
+        eth = {'dl_vlan': 100}
+        dpid = {'dpid': 'a', 'in_port': 1}
+        switch = {'switch': dpid, 'eth': eth}
+        entries = {'trace': switch}
+        self.trace_entries.load_entries(entries)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tracing/trace_entries.py
@@ -0,0 +1,349 @@
+"""
+    Class Entries. Used to evaluate entries provided.
+"""
+import re
+
+
+dpid_addr = re.compile('([0-9A-Fa-f]{2}[-:]){7}[0-9A-Fa-f]{2}$')
+mac_addr = re.compile('([0-9A-Fa-f]{2}[-:]){5}[0-9A-Fa-f]{2}$')
+ip_addr = re.compile("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+
+
+class TraceEntries(object):
+    """ Class Entries. Used to evaluate entries provided. """
+
+    def __init__(self):
+        self._dpid = None
+        self._in_port = None
+        self._dl_src = None
+        self._dl_dst = None
+        self._dl_vlan = None
+        self._dl_type = None
+        self._dl_vlan_pcp = None
+        self._nw_src = None
+        self._nw_dst = None
+        self._nw_tos = None
+        self._tp_src = None
+        self._tp_dst = None
+
+    @property
+    def dpid(self):
+        """ Property dpid """
+        return self._dpid
+
+    @dpid.setter
+    def dpid(self, dpid):
+        """ Validates DPID formats: '1', 'a',  '0000000000000001',
+        'abcdefabcdefabcd', 'ab:cd:ef:ab:cd:ef:ab:cd'
+
+        Args:
+            dpid: entries['trace']['switch']['dpid']
+        """
+        if not isinstance(dpid, str):
+            raise ValueError("Error: dpid has to be string")
+
+        if 0 < len(dpid) <= 16:  # pylint: disable=C1801
+            # DPIDs: '1', 'a', '0000000000000001', 'abcdefabcdefabcd'
+            if dpid.isalnum():
+                if not re.search('[g-z]', dpid.lower()):
+                    error_len = False
+                else:
+                    error_len = True
+            else:
+                error_len = True
+
+        elif len(dpid) == 23:
+            # DPIDs: 'ab:cd:ef:ab:cd:ef:ab:cd'
+            # Valid chars: 0-9, :, a-f, A-Z
+            if re.search(dpid_addr, dpid):
+                error_len = False
+            else:
+                error_len = True
+        else:
+            error_len = True
+
+        if error_len:  # pylint: disable=E0601
+            msg = "Error: dpid allows char [a-f], int, and :. Lengths: 1-16 and 23"
+            raise ValueError(msg)
+
+        self._dpid = dpid
+
+    @property
+    def in_port(self):
+        """ IN_PORT Getter """
+        return self._in_port
+
+    @in_port.setter
+    def in_port(self, in_port):
+        """ IN_PORT Setter """
+        if not isinstance(in_port, int):
+            raise ValueError("Error: in_port has to be integer")
+        else:
+            if in_port <= 0 or in_port >= 2**64:
+                raise ValueError("Error: in_port has to be > 0")
+
+        self._in_port = in_port
+
+    @property
+    def dl_src(self):
+        """ dl_src Getter """
+        return self._dl_src
+
+    @dl_src.setter
+    def dl_src(self, dl_src):
+        """ dl_src Setter: entries['trace']['eth']['dl_src'].
+        Validates MAC formats: 'ab:cd:ef:ab:cd:ef'
+
+        Args:
+            dl_src: entries['trace']['eth']['dl_src']
+        """
+
+        if not isinstance(dl_src, str):
+            raise ValueError("Error: dl_src has to be string")
+
+        elif not re.search(mac_addr, dl_src):
+            # DPIDs: 'ab:cd:ef:ab:cd:ef'
+            # Valid chars: 0-9, :, a-f, A-Z
+            msg = "Error: dl_src allows char [a-f], int, and :. Lengths: 17"
+            raise ValueError(msg)
+
+        self._dl_src = dl_src
+
+    @property
+    def dl_dst(self):
+        """ dl_dst Getter """
+        return self._dl_dst
+
+    @dl_dst.setter
+    def dl_dst(self, dl_dst):
+        """ dl_dst Setter: entries['trace']['eth']['dl_dst'].
+        Validates MAC formats: 'ab:cd:ef:ab:cd:ef'
+
+        Args:
+            dl_dst: entries['trace']['eth']['dl_dst']
+        """
+
+        if not isinstance(dl_dst, str):
+            raise ValueError("Error: dl_dst has to be string")
+
+        elif not re.search(mac_addr, dl_dst):
+            # DPIDs: 'ab:cd:ef:ab:cd:ef'
+            # Valid chars: 0-9, :, a-f, A-Z
+            msg = "Error: dl_dst allows char [a-f], int, and :. Lengths: 17"
+            raise ValueError(msg)
+
+        self._dl_dst = dl_dst
+
+    @property
+    def dl_vlan(self):
+        """ dl_vlan Getter """
+        return self._dl_vlan
+
+    @dl_vlan.setter
+    def dl_vlan(self, dl_vlan):
+        """ dl_vlan Setter """
+        if not isinstance(dl_vlan, int):
+            raise ValueError("Error: dl_vlan has to be integer")
+        else:
+            if not 0 < dl_vlan <= 4095:
+                raise ValueError("Error: dl_vlan has to be between 0 and 4095")
+
+        self._dl_vlan = dl_vlan
+
+    @property
+    def dl_type(self):
+        """ dl_type Getter """
+        return self._dl_type
+
+    @dl_type.setter
+    def dl_type(self, dl_type):
+        """ dl_type Setter """
+        if not isinstance(dl_type, int):
+            raise ValueError("Error: dl_type has to be integer")
+        else:
+            if not 0 < dl_type <= 65535:
+                raise ValueError("Error: dl_type has to be between 0 and 65535")
+
+        self._dl_type = dl_type
+
+    @property
+    def dl_vlan_pcp(self):
+        """ dl_vlan_pcp Getter """
+        return self._dl_vlan_pcp
+
+    @dl_vlan_pcp.setter
+    def dl_vlan_pcp(self, dl_vlan_pcp):
+        """ dl_vlan_pcp Setter """
+        if not isinstance(dl_vlan_pcp, int):
+            raise ValueError("Error: dl_vlan_pcp has to be integer")
+        else:
+            if not 0 < dl_vlan_pcp <= 7:
+                raise ValueError("Error: dl_vlan_pcp has to be between 0 and 7")
+
+        self._dl_vlan_pcp = dl_vlan_pcp
+
+    @property
+    def nw_tos(self):
+        """ nw_tos Getter """
+        return self._nw_tos
+
+    @nw_tos.setter
+    def nw_tos(self, nw_tos):
+        """ nw_tos Setter """
+        if not isinstance(nw_tos, int):
+            raise ValueError("Error: nw_tos has to be integer")
+        else:
+            if not 0 < nw_tos <= 7:
+                raise ValueError("Error: nw_tos has to be between 0 and 7")
+
+        self._nw_tos = nw_tos
+
+    @property
+    def nw_src(self):
+        """ nw_src Getter """
+        return self._nw_src
+
+    @nw_src.setter
+    def nw_src(self, nw_src):
+        """ nw_src Setter """
+
+        if not isinstance(nw_src, str):
+            raise ValueError("Error: nw_src has to be string")
+
+        elif not re.search(ip_addr, nw_src):
+            # Filters: 0.0.0.1 to 255.255.255.255
+            msg = "Error: nw_src is not a proper IPv4"
+            raise ValueError(msg)
+
+        self._nw_src = nw_src
+
+    @property
+    def nw_dst(self):
+        """ nw_dst Getter """
+        return self._nw_dst
+
+    @nw_dst.setter
+    def nw_dst(self, nw_dst):
+        """ nw_dst Setter """
+
+        if not isinstance(nw_dst, str):
+            raise ValueError("Error: nw_dst has to be string")
+
+        elif not re.search(ip_addr, nw_dst):
+            # Filters: 0.0.0.1 to 255.255.255.255
+            msg = "Error: nw_dst is not a proper IPv4"
+            raise ValueError(msg)
+
+        self._nw_dst = nw_dst
+
+    @property
+    def tp_src(self):
+        """ tp_src Getter """
+        return self._tp_src
+
+    @tp_src.setter
+    def tp_src(self, tp_src):
+        """ tp_src Setter """
+        if not isinstance(tp_src, int):
+            raise ValueError("Error: tp_src has to be integer")
+        else:
+            if not 0 < tp_src <= 65535:
+                raise ValueError("Error: tp_src has to be between 0 and 65535")
+
+        self._tp_src = tp_src
+
+    @property
+    def tp_dst(self):
+        """ tp_dst Getter """
+        return self._tp_dst
+
+    @tp_dst.setter
+    def tp_dst(self, tp_dst):
+        """ tp_dst Setter """
+        if not isinstance(tp_dst, int):
+            raise ValueError("Error: tp_dst has to be integer")
+        else:
+            if not 0 < tp_dst <= 65535:
+                raise ValueError("Error: tp_dst has to be between 0 and 65535")
+
+        self._tp_dst = tp_dst
+
+    def load_entries(self, entries):
+        """ Import entries provided
+
+        Args:
+            entries: user-provided entries
+        """
+        # Basic entries['trace']
+        if 'trace' not in entries:
+            raise ValueError("Error: Trace entry missing")
+        elif not isinstance(entries, dict):
+            raise ValueError("Error: switch has to be dict")
+
+        trace = entries['trace']
+
+        # Basic entries['trace']['switch']
+        if 'switch' not in trace:
+            raise ValueError("Error: switch not provided")
+        elif not isinstance(trace['switch'], dict):
+            raise ValueError("Error: switch has to be dict")
+
+        switch = trace['switch']
+
+        if 'dpid' not in switch:
+            raise ValueError("Error: dpid not provided")
+        else:
+            self.dpid = switch['dpid']
+
+        if 'in_port' not in switch:
+            raise ValueError("Error: in_port not provided")
+        else:
+            self.in_port = switch['in_port']
+
+        # Basic entries['trace']['switch']
+        if 'eth' not in trace:
+            raise ValueError("Error: eth not provided")
+        elif not isinstance(trace['eth'], dict):
+            raise ValueError("Error: eth has to be dict")
+
+        eth = trace['eth']
+
+        if 'dl_vlan' not in eth:
+            raise ValueError("Error: dl_vlan not provided")
+        else:
+            self.dl_vlan = eth['dl_vlan']
+
+        if 'dl_src' in eth:
+            self.dl_src = eth['dl_src']
+
+        if 'dl_dst' in eth:
+            self.dl_dst = eth['dl_dst']
+
+        if 'dl_vlan_pcp' in eth:
+            self.dl_vlan_pcp = eth['dl_vlan_pcp']
+
+        if 'dl_type' in eth:
+            self.dl_type = eth['dl_type']
+
+        # Basic entries['trace']['ip']
+        if 'ip' in trace:
+            ip_ = trace['ip']
+
+            if 'nw_src' in ip_:
+                self.nw_src = ip_['nw_src']
+
+            if 'nw_dst' in ip_:
+                self.nw_dst = ip_['nw_dst']
+
+            if 'nw_tos' in ip_:
+                self.nw_tos = ip_['nw_tos']
+
+        # Basic entries['trace']['ip']
+        if 'tp' in trace:
+            tp_ = trace['tp']
+
+            if 'tp_src' in tp_:
+                self.tp_src = tp_['tp_src']
+
+            if 'tp_dst' in tp_:
+                self.tp_dst = tp_['tp_dst']
