f4266d913bdfa90dad4dd5c767c36eb941c74946
Vinicius Arcanjo <viniarck@gmail.com>
2022-07-18 10:12:56 -0300

    Added test_send_packet_out on existing test suite

--- tests/unit/backends/test_openflow13.py
+++ tests/unit/backends/test_openflow13.py
@@ -1,6 +1,6 @@
 """ Tests for /backends/openflow13.py """
 from unittest import TestCase
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 from napps.amlight.sdntrace import settings
 from napps.amlight.sdntrace.backends.openflow13 import (
@@ -82,3 +82,11 @@ class TestOpenflow13(TestCase):
             called_event.content["message"].actions[0].port,
             settings.OFPP_TABLE_13,
         )
+
+    @patch("napps.amlight.sdntrace.backends.openflow13.of_msg_prio")
+    def test_send_packet_out(self, mock_of_msg_prio) -> None:
+        """Test send_packet_out."""
+        controller = MagicMock()
+        send_packet_out(controller, MagicMock(), MagicMock(), MagicMock())
+        assert controller.buffers.msg_out.put.call_count == 1
+        assert mock_of_msg_prio.call_count == 1
